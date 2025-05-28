761b3f883a2f8b77623a7b153d5ab07fc3e10168
Kairat Tobokelov <kajrattobokelov2@gmail.com>
2025-05-28 17:22:25 +0600

    - finished bloc

--- lib/data/repositories/connection_repository_impl.dart
+++ lib/data/repositories/connection_repository_impl.dart
@@ -1,25 +1,114 @@
 import 'dart:async';
 import 'dart:io';
+
+import 'package:web_socket_channel/status.dart' as status;
 import 'package:web_socket_channel/web_socket_channel.dart';
+
 import '../../domain/repositories/connection_repository.dart';
 
 class ConnectionRepositoryImpl implements ConnectionRepository {
   WebSocketChannel? _webSocketChannel;
   Socket? _tcpSocket;
+  StreamSubscription? _wsStreamSubscription;
 
   @override
   Future<void> connectWebSocket(String url) async {
-    _webSocketChannel = WebSocketChannel.connect(Uri.parse(url));
+
+    final completer = Completer<void>();
+    const Duration connectionTimeout = Duration(seconds: 7);
+
+    try {
+      _webSocketChannel = WebSocketChannel.connect(Uri.parse(url));
+
+      _wsStreamSubscription = _webSocketChannel!.stream.listen(
+            (message) {
+          if (!completer.isCompleted) {
+            completer.complete();
+          }
+        },
+        onError: (error) {
+          if (!completer.isCompleted) {
+            completer.completeError(Exception('WebSocket stream error: $error'));
+          }
+        },
+        onDone: () {
+          if (!completer.isCompleted) {
+            if (_webSocketChannel?.closeCode != null && _webSocketChannel?.closeCode != status.normalClosure) {
+              completer.completeError(Exception('WebSocket connection closed with code: ${_webSocketChannel?.closeCode}'));
+            } else {
+              completer.completeError(Exception('The WebSocket stream was closed without explicit confirmation of the connection being established'));
+            }
+          }
+        },
+      );
+
+      _webSocketChannel!.sink.done.then((_) {
+        if (!completer.isCompleted && _webSocketChannel?.closeCode != status.normalClosure) {
+          completer.completeError(Exception('Sink WebSocket finished with error, code: ${_webSocketChannel?.closeCode}'));
+        }
+      }).catchError((error) {
+        if (!completer.isCompleted) {
+          completer.completeError(Exception('Error Sink WebSocket: $error'));
+        }
+      });
+
+      await completer.future.timeout(connectionTimeout, onTimeout: () {
+        _wsStreamSubscription?.cancel();
+        _wsStreamSubscription = null;
+        _webSocketChannel?.sink.close(status.goingAway, 'Client timeout').catchError((_) {});
+        _webSocketChannel = null;
+        throw TimeoutException('WebSocket connection timeout', connectionTimeout);
+      });
+
+    } catch (e) {
+      await _wsStreamSubscription?.cancel();
+      _wsStreamSubscription = null;
+      _webSocketChannel?.sink.close().catchError((_){});
+      _webSocketChannel = null;
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> disconnectWebSocket() async {
+    if (_webSocketChannel == null && _wsStreamSubscription == null) {
+      return;
+    }
+    await _wsStreamSubscription?.cancel();
+    _wsStreamSubscription = null;
+    try {
+      await _webSocketChannel?.sink.close(status.normalClosure);
+    } catch (e) {}
+    _webSocketChannel = null;
   }
 
   @override
   Future<void> connectTcp(String host, int port) async {
-    _tcpSocket = await Socket.connect(host, port);
+    await disconnectTcp();
+
+    try {
+      _tcpSocket = await Socket.connect(host, port, timeout: const Duration(seconds: 7));
+    } catch (e) {
+      _tcpSocket = null;
+      rethrow;
+    }
   }
 
   @override
-  Future<void> disconnect() async {
-    await _webSocketChannel?.sink.close();
-    _tcpSocket?.destroy();
+  Future<void> disconnectTcp() async {
+    if (_tcpSocket == null) {
+      return;
+    }
+    try {
+      _tcpSocket?.destroy();
+    } catch (e) {}
+    _tcpSocket = null;
   }
+
+  @override
+  Future<void> disconnectAll() async {
+    await disconnectWebSocket();
+    await disconnectTcp();
+  }
+
 }
--- lib/domain/repositories/connection_repository.dart
+++ lib/domain/repositories/connection_repository.dart
@@ -1,5 +1,7 @@
 abstract class ConnectionRepository {
   Future<void> connectWebSocket(String url);
+  Future<void> disconnectWebSocket();
   Future<void> connectTcp(String host, int port);
-  Future<void> disconnect();
+  Future<void> disconnectTcp();
+  Future<void> disconnectAll();
 }
